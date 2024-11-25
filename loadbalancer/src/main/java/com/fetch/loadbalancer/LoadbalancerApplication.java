0f3ca384119f196459cf3d15b939e4fa936828b1
Joey Norkin <joey.norkin@gmail.com>
2024-11-25 16:52:25 -0500

    Refactor spring cloud gateway routes

--- loadbalancer/src/main/java/com/fetch/loadbalancer/LoadbalancerApplication.java
+++ loadbalancer/src/main/java/com/fetch/loadbalancer/LoadbalancerApplication.java
@@ -2,12 +2,24 @@ package com.fetch.loadbalancer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LoadbalancerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LoadbalancerApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("myRoute", p -> p
+                        .path("/receipts/**")
+                        .uri("lb://receipt-processor"))
+                .build();
     }
 
 }
